92abd7d4b2493ac12174f4e16d0d3ce38fbd3b35
Alan Yang <me@alanyang.com>
2022-04-19 12:09:50 -0400

    feat; add ReactQueryDevtools

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { worker } from '@uidotdev/react-query-api';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from '@/App';
@@ -24,6 +25,7 @@ new Promise((res) => setTimeout(res, 100))
     root.render(
       <StrictMode>
         <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools />
           <BrowserRouter>
             <App />
           </BrowserRouter>
