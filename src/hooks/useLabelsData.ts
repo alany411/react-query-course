975e0c4d9cadf4446e4b531e425f164afb059ca9
Alan Yang <me@alanyang.com>
2022-04-19 16:15:35 -0400

    feat: add placeholderData to Labels

--- src/hooks/useLabelsData.ts
+++ src/hooks/useLabelsData.ts
@@ -1,10 +1,12 @@
 import { useQuery } from 'react-query';
 
+import { defaultLabels } from '@/helpers/defaultData';
 import fetchWithError from '@/helpers/fetchWithError';
 
 export default function useLabelsData() {
   const labelsQuery = useQuery<Label[], Error>(['labels'], ({ signal }) => fetchWithError('/api/labels', { signal }), {
     staleTime: 1000 * 60 * 60,
+    placeholderData: defaultLabels,
   });
 
   return labelsQuery;
